4ae03ab5da5fed3a21d13e713f98ca71dc21d479
RespiaMaple <ian9025623931@gmail.com>
2023-04-17 23:54:30 +0800

    1

--- bot.py
+++ bot.py
@@ -36,6 +36,22 @@ async def on_message(msg):
         pic = discord.File(jdata["D:"])
     elif msg.content == "nlnlouo" :
         pic = discord.File(jdata["nlnlouo"])
+    elif msg.content == "俊宏愛你" :
+        pic = discord.File(jdata["俊宏愛你"])
+    elif msg.content == "累了" :
+        pic = discord.File(jdata["累了"])
+    elif msg.content == "舔舔" :
+        pic = discord.File(jdata["舔舔"])
+    elif msg.content == "..." :
+        pic = discord.File(jdata["..."])
+    elif msg.content == "aquasmoke" :
+        pic = discord.File(jdata["aquasmoke"])
+    elif msg.content == "讚" :
+        pic = discord.File(jdata["讚"])
+    elif msg.content == "penis good" :
+        pic = discord.File(jdata["penis good"])
     await msg.channel.send(file=pic)
+
+
 
 bot.run(jdata["TOKEN"])
