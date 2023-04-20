deb33c5cc46d241b84178fb16f294579c92d6d98
RespiaMaple <ian9025623931@gmail.com>
2023-04-20 21:37:37 +0800

    1

--- bot.py
+++ bot.py
@@ -27,6 +27,24 @@ async def on_member_remove(member):
 async def ping(ctx):                                #ctx(上下文)，回覆上下文所在的頻道
     await ctx.send(f"{round(bot.latency*1000)} (ms)")
 
+@bot.command()
+async def roll(ctx):
+  await ctx.send(f"{random.randint(1,6)}點")
+
+@bot.command()
+async def flip(ctx):
+  coin_side = ["正面", "反面"]
+  coin = random.choice(coin_side)
+  await ctx.send(f"{coin}")
+
+@bot.command()
+async def cmd(ctx):
+    keyword_dict=["bbbb87cry","nlnlsofun","D:","nlnlouo","俊宏愛你","累了","舔舔","..."
+                  ,"aquasmoke","讚","penis good"]
+    commands_list = "\n".join([f"`{c.name}`" for c in bot.commands])
+    keywords_list = "\n".join([f"`{k}`" for k in keyword_dict])
+    await ctx.send(f"指令列表:\n{commands_list}\n\n貼圖列表:\n{keywords_list}")    
+
 @bot.event
 async def on_message(msg):
     keyword_dict = {
@@ -42,15 +60,12 @@ async def on_message(msg):
         "讚": jdata["讚"],
         "penis good": jdata["penis good"]
     }
-
     greetings = ["你好", "哈囉", "安安"]
-
     pic = None
     for keyword in keyword_dict:
-        if keyword in msg.content:
+        if keyword in msg.content and msg.author!=bot.user:
             pic = discord.File(keyword_dict[keyword])
-            break
-    
+            break  
     if any(keyword in msg.content for keyword in greetings) and msg.author!=bot.user:
         greeting = random.choice(greetings)
         await msg.channel.send(f"{msg.author.mention} {greeting}!")
@@ -59,9 +74,10 @@ async def on_message(msg):
         await msg.channel.send(f"{user.mention}不要再玩原神了!")
     elif any(keyword in msg.content for keyword in ["日麻", "雀魂"]) and msg.author!=bot.user:
         await msg.channel.send("@everyone 該搓日麻了吧!")
-
     if pic:
         await msg.channel.send(file=pic)
+    await bot.process_commands(msg)
+
 
 
 bot.run(jdata["TOKEN"])
